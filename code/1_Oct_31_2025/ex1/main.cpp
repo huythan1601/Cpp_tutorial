5a41f4809f88401b7891104113af785fd4e320ab
HuyThan <hoanghuybaby2002@gmail.com>
2025-11-02 22:00:21 +0700

    add source code of ex1

---
+++ code/1_Oct_31_2025/ex1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  // Include the iostream library for input/output operations
+#include "test.h"
+
+int main(void)  // The main function, where program execution begins
+{
+    std::cout << "Hello world" << std::endl;  // Output "Hello World!" to the console
+    test();
+    return 0;  // Indicate successful program execution
+}
+
+
---
+++ code/1_Oct_31_2025/ex1/src/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "test.h"
+
+void test()
+{
+    std::cout << "This is test line\n";
+}
