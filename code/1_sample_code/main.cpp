dedef0530705f22407eefbfbb57d1a9c6e4002e2
HuyThan <hoanghuybaby2002@gmail.com>
2025-11-06 23:38:14 +0700

    edit commit in source code

--- code/1_sample_code/main.cpp
+++ code/1_sample_code/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>  // Include the iostream library for input/output operations
-// #include "test.h"
 
-int main(void)  // The main function, where program execution begins
+/* 
+The main function, where program execution begins. 
+*/
+
+int main(void)  
 {
     std::cout << "Hello world" << std::endl;  // Output "Hello World!" to the console
     return 0;  // Indicate successful program execution
