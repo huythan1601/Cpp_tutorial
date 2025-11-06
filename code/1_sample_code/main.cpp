9d008549e8b3c4d5624300e1386deeba05f6a1da
HuyThan <hoanghuybaby2002@gmail.com>
2025-11-06 23:39:18 +0700

    using namespace std

--- code/1_sample_code/main.cpp
+++ code/1_sample_code/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>  // Include the iostream library for input/output operations
 
+using namespace std;
 /* 
 The main function, where program execution begins. 
 */
 
 int main(void)  
 {
-    std::cout << "Hello world" << std::endl;  // Output "Hello World!" to the console
+    cout << "Hello world" << endl;  // Output "Hello World!" to the console
     return 0;  // Indicate successful program execution
 }
 
