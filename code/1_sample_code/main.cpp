af35abfd4ab85a9977586cdfe9af1209a775641b
tamnguyen <treetam100@gmail.com>
2025-11-09 20:54:21 +0700

    add bye world

--- code/1_sample_code/main.cpp
+++ code/1_sample_code/main.cpp
@@ -8,8 +8,8 @@ The main function, where program execution begins.
 int main(void)  
 {
     cout << "Hello world" << endl;  // Output "Hello World!" to the console
+    cout << "Bye World"
 
-    
     return 0;  // Indicate successful program execution
 }
 
