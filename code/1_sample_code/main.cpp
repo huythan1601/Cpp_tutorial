f6ae5e05bdf363cb405203c7119bb37d23b064d9
tamnguyen <treetam100@gmail.com>
2025-11-11 09:52:52 +0700

    0_excercise

--- code/1_sample_code/main.cpp
+++ code/1_sample_code/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>  // Include the iostream library for input/output operations
-
+#include <string>
 using namespace std;
 /* 
 The main function, where program execution begins. 
@@ -7,7 +7,11 @@ The main function, where program execution begins.
 
 int main(void)  
 {
-    cout << "Hello world" << endl;  // Output "Hello World!" to the console
+    string First_Name = "Tam";
+    cout <<"First_Name:" << First_Name << endl;
+    cout <<"Name: Nguyen Tri Tam" << endl;
+    cout <<"Age: 35 years old" << endl;
+    cout <<"Nationality: Vietnamese" << endl;
     return 0;  // Indicate successful program execution
 }
 
