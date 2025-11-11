c009058bd86a9fb91e803045f26db86080f6c34a
tamnguyen <treetam100@gmail.com>
2025-11-11 10:08:00 +0700

    0_excercise

---
+++ code/0_exercise/1_Display/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+#include <string>
+using namespace std;
+int main(void)  
+{
+    string myName = "Nguyen_Tri_Tam";
+    cout <<"myName:" << myName << endl;
+    string myAge = "35";
+    cout <<"myAge:" << myAge << endl;
+    string My_Nationality = "Vietnamese";
+    cout <<"My_Nationality:" << My_Nationality << endl;
+}
