dd3fc8825fdaa1a205ac4b56e7d5a8e721293671
tamnguyen <treetam100@gmail.com>
2025-11-12 20:17:33 +0700

    2_advanced_display

---
+++ code/0_exercise/2_advanced_display/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main (void)
+
+{
+string My_Name = "Nguyen Tri Tam";
+string My_Age = "35";
+string My_Nationality = "Vietnamese";
+
+cout <<"Hello everyone, my name is " <<My_Name<< " I am "  <<My_Age<< " years old and my nationality is " <<My_Nationality<< endl;
+return 0; 
+
+
+}
+
+
+
+
